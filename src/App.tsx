aef493e3a40e1d8c99fa496aa83c199a00eb7957
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2023-04-21 16:39:51 -0300

    Estrutura da aplicação - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
