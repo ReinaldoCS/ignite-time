c49a07f577e13591acd9625599b0468c798c8ea3
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2023-05-09 20:45:53 -0300

    Páginas e rotas - Componente: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryLis } from './styles'
+import { HistoryContainer, HistoryLis, Status } from './styles'
 
 export function History() {
   return (
@@ -19,25 +19,25 @@ export function History() {
               <td>Conserto de débitos técnicos</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Conserto de débitos técnicos</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Conserto de débitos técnicos</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Em andamento</td>
-            </tr>
-            <tr>
-              <td>Conserto de débitos técnicos</td>
-              <td>25 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
